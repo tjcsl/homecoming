e4046fa66de497e4c98a875c494594886741ed55
jamesthoughton <jamesthoughton@gmail.com>
2016-09-10 17:39:59 -0400

    Add some OAUTH stuff to settings and templates

--- hocoapp/apps/base/views.py
+++ hocoapp/apps/base/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def index_view(request):
     if request.user.is_authenticated():
         # Do something
         pass
 
-    context = {}
+    oauth_href = "https://ion.tjhsst.edu/oauth/authorize" + \
+                 "?response_type=code" + \
+                 "&client_id={}".format(settings.OAUTH_CLIENT_ID) + \
+                 "&redirect_uri={}".format(settings.OAUTH_REDIRECT_URI) + \
+                 "&scope=read&state=".format(settings.OAUTH_STATE)
+
+    context = {
+        "oauth_href": oauth_href
+    }
     return render(request, 'landing.html', context)
