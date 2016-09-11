f71849811e61561a1951d41eb2c3127c5d5195fa
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-09-10 22:30:50 -0400

    create Event and ScoreBoard model

--- hocoapp/apps/events/models.py
+++ hocoapp/apps/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+
+    """A Model for an Event in Homecoming Week"""
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=512)
+    time = models.DateTimeField()
+
+    @classmethod
+    def create(cls, name=None, description=None, time=None):
+        event = cls(name=name, description=description, time=time)
+        return event
--- hocoapp/apps/scores/models.py
+++ hocoapp/apps/scores/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from ..events.models import Event
+
 # Create your models here.
+
+
+class ScoreBoard(models.Model):
+
+    """A ScoreBoard keeps track of an Event.
+    It has a score field for all 4 classes"""
+
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    freshman_score = models.DecimalField(max_digits=10, decimal_places=2)
+    sophomore_score = models.DecimalField(max_digits=10, decimal_places=2)
+    junior_score = models.DecimalField(max_digits=10, decimal_places=2)
+    senior_score = models.DecimalField(max_digits=10, decimal_places=2)
+
+    @classmethod
+    def create(cls, event=None, freshman_score=None, sophomore_score=None, junior_score=None, senior_score=None):
+        scoreboard = cls(event=event, freshman_score=freshman_score,
+                         sophomore_score=sophomore_score, junior_score=junior_score, senior_score=senior_score)
+        return scoreboard
