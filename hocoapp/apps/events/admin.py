6a7afce552eea9581c4561d7f12bf7a211c8b461
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-09-11 09:56:14 -0400

    register admin sites

--- hocoapp/apps/events/admin.py
+++ hocoapp/apps/events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Event, EventAdmin)
--- hocoapp/apps/scores/admin.py
+++ hocoapp/apps/scores/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import ScoreBoard
+
 # Register your models here.
+
+
+class ScoreBoardAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ScoreBoard, ScoreBoardAdmin)
