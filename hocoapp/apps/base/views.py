1b027aad80ef5afcdf74709087051665f94aafc6
Eric Wang <ezwang1@gmail.com>
2016-09-23 16:01:07 -0400

    add total

--- hocoapp/apps/base/views.py
+++ hocoapp/apps/base/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.conf import settings
 from django.urls import reverse
 
+from django.db.models import Sum
+
 from hocoapp.decorators import login_required
 from hocoapp.apps.events.models import Event
 from hocoapp.apps.scores.models import ScoreBoard
@@ -19,6 +21,10 @@ def index_view(request):
     context = {
         "events": events,
         "schedule": schedule,
-        "scoreboards": scoreboards
+        "scoreboards": scoreboards,
+        "freshman_total": ScoreBoard.objects.aggregate(Sum("freshman_score"))["freshman_score__sum"],
+        "sophomore_total": ScoreBoard.objects.aggregate(Sum("sophomore_score"))["sophomore_score__sum"],
+        "junior_total": ScoreBoard.objects.aggregate(Sum("junior_score"))["junior_score__sum"],
+        "senior_total": ScoreBoard.objects.aggregate(Sum("senior_score"))["senior_score__sum"],
     }
     return render(request, 'home.html', context)
