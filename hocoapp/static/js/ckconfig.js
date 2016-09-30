a875ef17a4dd88c91dd8817593035815571a1446
Eric Wang <ezwang1@gmail.com>
2016-09-30 19:55:12 -0400

    add html descriptions

---
+++ hocoapp/static/js/ckconfig.js
@@ -0,0 +1,11 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.toolbar = [
+        { name: 'clipboard', items: ['Undo', 'Redo' ] },
+        { name: 'links', items: [ 'Link', 'Unlink' ] },
+        { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
+        { name: 'tools', items: [ 'Maximize' ] },
+        { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+        { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
+        { name: 'styles', items: [ 'Styles', 'Format' ] }
+    ];
+};
