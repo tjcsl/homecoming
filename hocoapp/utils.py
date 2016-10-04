4a9a88afd360f6752d75251aff8d410316cdaeb5
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-10-03 22:21:33 -0400

    stuff

--- hocoapp/utils.py
+++ hocoapp/utils.py
@@ -5,5 +5,5 @@ def is_authenticated(req):
 def is_admin(req):
     username = req.session.get("uid")
     if username:
-        return username in ["2019okulkarn", "2017jhoughto", "2017ewang"]
+        return username in ["2019okulkarn", "2017jhoughto", "2017ewang", "2018kdu", "2017zli", "2018sxie", "2018jprabhal", "2017gvaldeta", "2017ycho"]
     return False
