dded2831f31c519bf1d4c2133430f8e97accfb26
Eric Wang <2017ewang@tjhsst.edu>
2016-10-11 20:14:21 -0400

    commit random changes

--- hocoapp/apps/base/views.py
+++ hocoapp/apps/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.urls import reverse
-
+from django.http import JsonResponse
 from django.db.models import Sum
 
 from hocoapp.decorators import login_required
@@ -16,7 +16,7 @@ def index_view(request):
 
     events = Event.objects.all().order_by("start_time")
     schedule = "schedule"
-    scoreboards = ScoreBoard.objects.all()
+    scoreboards = ScoreBoard.objects.all().order_by("event__start_time")
 
     context = {
         "events": events,
@@ -28,3 +28,15 @@ def index_view(request):
         "senior_total": ScoreBoard.objects.aggregate(Sum("senior_score"))["senior_score__sum"],
     }
     return render(request, 'home.html', context)
+
+def api_view(request):
+    context = {
+        "freshman_total": ScoreBoard.objects.aggregate(Sum("freshman_score"))["freshman_score__sum"],
+        "sophomore_total": ScoreBoard.objects.aggregate(Sum("sophomore_score"))["sophomore_score__sum"],
+        "junior_total": ScoreBoard.objects.aggregate(Sum("junior_score"))["junior_score__sum"],
+        "senior_total": ScoreBoard.objects.aggregate(Sum("senior_score"))["senior_score__sum"],
+    }
+    resp = JsonResponse(context)
+    resp["Access-Control-Allow-Origin"] = "*"
+    resp["Access-Control-Allow-Headers"] = "x-requested-with"
+    return resp
