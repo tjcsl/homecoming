60aca8843b9a9d03f3e49a8a470ab417b5d4919b
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-11-04 19:09:15 -0400

    idk what this is

--- hocoapp/settings/__init__.py
+++ hocoapp/settings/__init__.py
@@ -21,7 +21,7 @@ PROJECT_ROOT = BASE_DIR
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'key'
+SECRET_KEY = 'this is a secrety key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
