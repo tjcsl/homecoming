7fe3f9f9848110bc06f2278b7246f518c325755a
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-14 23:26:47 -0500

    test(events): revise events tests

--- homecoming/apps/events/tests.py
+++ homecoming/apps/events/tests.py
@@ -1,7 +1,7 @@
 import datetime
 import json
 
-from django.shortcuts import reverse
+from django.urls import reverse
 from django.utils import timezone
 
 from ...test.homecoming_test import HomecomingTestCase
@@ -25,6 +25,21 @@ class EventsTestCase(HomecomingTestCase):
                 "location": "200C",
                 "start_time": "2020/11/07 21:00",
                 "end_time": "2020/11/08 21:00",
+            },
+        )
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, len(Event.objects.filter(name="Test Activity")))
+
+        # Test start time > end time
+        response = self.client.post(
+            reverse("events:create_event_page"),
+            follow=True,
+            data={
+                "name": "Test Activity",
+                "description": "Hello there!",
+                "location": "200C",
+                "start_time": "2020/11/07 21:00",
+                "end_time": "2020/11/06 21:00",
             },
         )
         self.assertEqual(200, response.status_code)
@@ -79,3 +94,14 @@ class EventsTestCase(HomecomingTestCase):
             },
             response,
         )
+
+    def test_event_model(self):
+        # Only because I like 100% coverage. :)
+        event = Event.create(
+            name="test123!",
+            description="test",
+            location="200D",
+            start_time=timezone.now(),
+            end_time=timezone.now() + datetime.timedelta(1),
+        )
+        self.assertEqual("test123!", str(event))
