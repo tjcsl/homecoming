9d20bab9a1fec8cf118b46c425b0c9b405841bf3
Ethan Nguyen <etnguyen03@hotmail.com>
2021-02-14 23:27:01 -0500

    test(scores): revise scores tests

--- homecoming/apps/scores/tests.py
+++ homecoming/apps/scores/tests.py
@@ -51,6 +51,26 @@ class ScoresTestCase(HomecomingTestCase):
             ),
         )
 
+        # Test invalid values
+        # End date before start date
+        response = self.client.post(
+            reverse("scores:edit_scores", kwargs={"event_id": event.id}),
+            follow=True,
+            data={
+                "name": "test-modify",
+                "description": "haha",
+                "location": event.location,
+                "start_time": event.start_time,
+                "end_time": event.start_time - timezone.timedelta(days=1),
+                "event_info": True,
+            },
+        )
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            1, len(Event.objects.filter(end_time=event.end_time, name="test-modify"))
+        )
+
         # Test editing scores
         response = self.client.post(
             reverse("scores:edit_scores", kwargs={"event_id": event.id}),
@@ -69,3 +89,36 @@ class ScoresTestCase(HomecomingTestCase):
         self.assertEqual(2, ScoreBoard.objects.get(id=scoreboard.id).sophomore_score)
         self.assertEqual(3, ScoreBoard.objects.get(id=scoreboard.id).junior_score)
         self.assertEqual(4, ScoreBoard.objects.get(id=scoreboard.id).senior_score)
+
+        # Test an invalid value
+        response = self.client.post(
+            reverse("scores:edit_scores", kwargs={"event_id": event.id}),
+            follow=True,
+            data={
+                "freshman_score": "sdlkj2",
+                "sophomore_score": "sdlf",
+                "junior_score": "haha",
+                "senior_score": "sdfd",
+                "event_score": True,
+            },
+        )
+
+        self.assertEqual(200, response.status_code)
+        # i.e. assert nothing changed
+        self.assertEqual(1, ScoreBoard.objects.get(id=scoreboard.id).freshman_score)
+        self.assertEqual(2, ScoreBoard.objects.get(id=scoreboard.id).sophomore_score)
+        self.assertEqual(3, ScoreBoard.objects.get(id=scoreboard.id).junior_score)
+        self.assertEqual(4, ScoreBoard.objects.get(id=scoreboard.id).senior_score)
+
+    def test_scoreboard_model(self):
+        # Only because I like 100% coverage. :)
+        event = Event.objects.create(
+            name="test1234!",
+            description="test",
+            location="200D",
+            start_time=timezone.now(),
+            end_time=timezone.now() + datetime.timedelta(1),
+        )
+
+        scoreboard = ScoreBoard.create(event=event)
+        self.assertEqual("test1234!", str(scoreboard))
