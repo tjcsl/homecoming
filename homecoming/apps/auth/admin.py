6b598353e45b6f13df5bdb44e536d4b395cb7209
Alan Zhu <2025azhu@tjhsst.edu>
2024-09-19 23:16:42 -0400

    feat(auth): add user admin

--- homecoming/apps/auth/admin.py
+++ homecoming/apps/auth/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import ClassGroup, User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "is_staff", "is_hoco_admin", "is_class_group_admin"]
+    list_filter = ["is_hoco_admin", "is_class_group_admin", "is_staff", "is_superuser", "is_student", "is_teacher"]
+    search_fields = ["username", "first_name", "last_name"]
+
+admin.site.register(User, UserAdmin)
 admin.site.register(ClassGroup)
